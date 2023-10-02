c11fc5601761485bf260d6a8b10737c8a263cc18
Jonathan Ballet <jon@multani.info>
2023-10-02 23:02:56 +0200

    wip

--- samples/client/echo_api/python/test/test_rest.py
+++ samples/client/echo_api/python/test/test_rest.py
@@ -5,21 +5,21 @@ from openapi_client.rest import RESTClientObject
 from openapi_client.configuration import Configuration
 
 
+class FakeResponse:
+    def __init__(self, status, reason, data):
+        self.status = status
+        self.reason = reason
+        self.data = data
+
 
 class TestRESTClient(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_1(self):
+    def test_get(self):
         config = Configuration()
         client = RESTClientObject(config)
         url = "http://localhost/test"
-
-        class FakeResponse:
-            def __init__(self, status, reason, data):
-                self.status = status
-                self.reason = reason
-                self.data = data
 
         with patch.object(client.pool_manager, "request") as mock:
             mock.return_value = FakeResponse(200, "OK", "hello")
@@ -27,4 +27,32 @@ class TestRESTClient(unittest.TestCase):
 
         self.assertEqual(response.data, "hello")
         self.assertEqual(response.status, 200)
-        mock.assert_called_once_with("GET", url)
+        mock.assert_called_once_with("GET", url, preload_content=True, timeout=None, headers={})
+
+
+    def test_post(self):
+        config = Configuration()
+        client = RESTClientObject(config)
+        url = "http://localhost/test"
+
+        with patch.object(client.pool_manager, "request") as mock:
+            mock.return_value = FakeResponse(200, "OK", "hello")
+            response = client.post_request(url)
+
+        self.assertEqual(response.data, "hello")
+        self.assertEqual(response.status, 200)
+        mock.assert_called_once_with("POST", url, preload_content=True, timeout=None, headers={}, body=None)
+
+
+    def test_post_with_params(self):
+        config = Configuration()
+        client = RESTClientObject(config)
+        url = "http://localhost/test"
+
+        with patch.object(client.pool_manager, "request") as mock:
+            mock.return_value = FakeResponse(200, "OK", "hello")
+            response = client.post_request(url, post_params={"foo": "bar"})
+
+        self.assertEqual(response.data, "hello")
+        self.assertEqual(response.status, 200)
+        mock.assert_called_once_with("POST", url, preload_content=True, timeout=None, headers={}, body=None, fields={"foo": "bar"})
