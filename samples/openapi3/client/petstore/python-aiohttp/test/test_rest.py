27b199eb79c380fd99374b88b7d14acdc090171b
Jonathan Ballet <jon@multani.info>
2023-10-08 22:37:32 +0200

    wip

--- samples/openapi3/client/petstore/python-aiohttp/test/test_rest.py
+++ samples/openapi3/client/petstore/python-aiohttp/test/test_rest.py
@@ -1,3 +1,5 @@
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import unittest
 from unittest.mock import patch
 from unittest.mock import AsyncMock
@@ -16,22 +18,48 @@ class FakeResponse:
         return self._data
 
 
+
+
+
 class TestRESTClient(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
-        pass
+        class Handler(BaseHTTPRequestHandler):
+            pass
+
+        server_address = ('', 8000)
+        self.httpd = HTTPServer(server_address, Handler)
+
+        self.thread = threading.Thread(target=self.httpd.serve_forever)
+        self.thread.start()
+
+    def tearDown(self):
+        print("shutting down")
+        self.httpd.shutdown()
+        self.thread.join()
+        print("shut down")
+
 
     async def test_get(self):
         config = Configuration()
         client = RESTClientObject(config)
-        url = "http://localhost/test"
+        url = "http://localhost:8000"
 
-        with patch.object(client.pool_manager, "request", new=AsyncMock()) as mock:
-            mock.return_value = FakeResponse(200, "OK", "hello")
-            response = await client.get_request(url)
 
-        self.assertEqual(response.data, "hello")
+        class Handler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b"hello")
+        
+        self.httpd.RequestHandlerClass = Handler
+
+        # with patch.object(client.pool_manager, "request", new=AsyncMock()) as mock:
+            # mock.return_value = FakeResponse(200, "OK", "hello")
+        response = await client.get_request(url)
+
+        self.assertEqual(response.data, b"hello")
         self.assertEqual(response.status, 200)
-        mock.assert_called_once_with("GET", url, timeout=300, headers={'Content-Type': 'application/json'})
+        # mock.assert_called_once_with("GET", url, timeout=300, headers={'Content-Type': 'application/json'})
 
 
     async def test_post(self):
